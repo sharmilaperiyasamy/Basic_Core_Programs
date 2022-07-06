31969f8abd496e03072600e3eb1ee2a021792b26
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-06 21:57:30 +0530

    [Sharmila] leap year

---
+++ Basic_Core/Leap_Year.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core
+{
+    internal class Leap_Year
+    {
+        public void Leap()
+        {
+            Console.WriteLine("Enter the year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            int yearLength = year.ToString().Length;
+
+            if (yearLength == 4)
+            {
+                if ((year % 4 == 0) && (year % 100 != 0))
+                {
+                    Console.WriteLine(year + " is a Leap year");
+                }
+                else if (year % 400 == 0)
+                {
+                    Console.WriteLine(year + " is a Leap year");
+                }
+                else
+                {
+                    Console.WriteLine(year + " is not a Leap year");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter the 4 digit number");
+            }
+        }
+    }
+}
--- Basic_Core/Program.cs
+++ Basic_Core/Program.cs
@@ -1,2 +1,5 @@
 ﻿Basic_Core.Flip_Coin coin = new Basic_Core.Flip_Coin();
 coin.Flip();
+
+Basic_Core.Leap_Year leap = new Basic_Core.Leap_Year();
+leap.Leap();
